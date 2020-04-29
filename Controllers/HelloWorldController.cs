82f9bf7ea7a38d7b562ca94006f733d523fb5cb4
Wendreo Luciano Fernandes <wendreofernandes@credisan.com.br>
2020-04-29 16:20:06 -0300

    1st...

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        // public string Index()
+        // {
+        //     return "This is my default action... hahahaha";
+        // }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // public string Welcome()
+        // {
+        //     return "This is the Welcome action method...";
+        // }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
